61b14c0dec9b75b29338aef19a529cf5c8c1595e
Chris Cutts <github@chriscutts.uk>
2025-01-16 01:17:26 +0000

    Set default ReactionPolicy methods to always return true.
    This change ensures all ReactionPolicy authorization methods default to allowing actions. It simplifies initial development and testing but should be revisited to implement proper permissions.

--- app/Policies/ReactionPolicy.php
+++ app/Policies/ReactionPolicy.php
@@ -12,30 +12,36 @@ class ReactionPolicy
 
     public function viewAny(User $user): bool
     {
-
+        return true;
     }
 
     public function view(User $user, Reaction $reaction): bool
     {
+        return true;
     }
 
     public function create(User $user): bool
     {
+        return true;
     }
 
     public function update(User $user, Reaction $reaction): bool
     {
+        return true;
     }
 
     public function delete(User $user, Reaction $reaction): bool
     {
+        return true;
     }
 
     public function restore(User $user, Reaction $reaction): bool
     {
+        return true;
     }
 
     public function forceDelete(User $user, Reaction $reaction): bool
     {
+        return true;
     }
 }
